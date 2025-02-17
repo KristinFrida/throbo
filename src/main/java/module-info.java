23386e0bab2668027eadd2a506bcead1500a9c70
KristinFrida <kfs14@hi.is>
2025-02-17 13:28:45 +0000

    Næ ég að setja þetta inn

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module is.hi.hbv401g.throunnhugbunadar {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens is.hi.hbv401g.throunnhugbunadar to javafx.fxml;
+    exports is.hi.hbv401g.throunnhugbunadar;
+}
