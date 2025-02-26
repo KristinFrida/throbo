2efff34828654d68acc788e7bd70bf660b3792a5
Edil <eik21@hi.is>
2025-02-26 17:52:43 +0000

    Bætti við breytum fyrir nöfn og myndir af tours

--- src/main/java/bakendi/Tour.java
+++ src/main/java/bakendi/Tour.java
@@ -1,4 +1,36 @@
 package bakendi;
 
 public class Tour {
+    private String name;
+    private String mainImage, image2, image3;
+    private String shortDescription, longDescription;
+    private String startLocation;
+    private double duration;
+    private int minAge;
+
+    // Constructor
+    public Tour(String name, String mainImage, String image2, String image3,
+                String shortDescription, String startLocation, double duration,
+                int minAge, String longDescription) {
+        this.name = name;
+        this.mainImage = mainImage;
+        this.image2 = image2;
+        this.image3 = image3;
+        this.shortDescription = shortDescription;
+        this.startLocation = startLocation;
+        this.duration = duration;
+        this.minAge = minAge;
+        this.longDescription = longDescription;
+    }
+
+    // Getters
+    public String getName() { return name; }
+    public String getMainImage() { return mainImage; }
+    public String getImage2() { return image2; }
+    public String getImage3() { return image3; }
+    public String getShortDescription() { return shortDescription; }
+    public String getStartLocation() { return startLocation; }
+    public double getDuration() { return duration; }
+    public int getMinAge() { return minAge; }
+    public String getLongDescription() { return longDescription; }
 }
