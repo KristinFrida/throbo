f2d9a17ecbab3c045cc2c1c26e12b5988fd1fd61
KristinFrida <kfs14@hi.is>
2025-02-28 12:58:33 +0000

    uppfærði checkbox

--- src/main/java/bakendi/Tour.java
+++ src/main/java/bakendi/Tour.java
@@ -10,7 +10,7 @@ public class Tour {
     private String startLocation;
     private double duration;
     private int minAge;
-    private double verdBilCheck;
+    private int verdBilCheck;
 
     /**
      * Smíðir tour hluti
