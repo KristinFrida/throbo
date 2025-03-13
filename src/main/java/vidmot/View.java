31581c1ed0c8f61cd387e4ea98d3450b834fcdb5
KristinFrida <kfs14@hi.is>
2025-03-13 19:36:29 +0000

    núna förum við á my-page

--- src/main/java/vidmot/View.java
+++ src/main/java/vidmot/View.java
@@ -7,7 +7,8 @@ public enum View {
     START("/vidmot/hello-view.fxml"),
     LOGIN("login-view.fxml"),
     SIGNUP("sign-in-view.fxml"),
-    TOUR_DETAILS("/vidmot/tour-details-view.fxml");
+    TOUR_DETAILS("/vidmot/tour-details-view.fxml"),
+    MYPAGE("my-page.fxml");
 
     private final String fileName;
 
