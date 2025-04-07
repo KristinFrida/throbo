1f24f83e468e4809597b971aca01f75f21fd988a
Sylvía Hanna Tyrfingsdóttir <sylviatyrfingsdottir@gmail.com>
2025-04-07 21:02:33 +0000

    ekki hægt að velja liðnar dagsetningar

--- src/main/java/vidmot/HelloController.java
+++ src/main/java/vidmot/HelloController.java
@@ -53,7 +53,19 @@ public class HelloController {
         assert datePicker != null : "Datepicker is not injected";
         assert fxLeitarvelTexti != null : "LeitarvelTexti is not injected";
         assert fxLoginTakki != null : "LoginTakki is not injected";
-        refreshLoginState();
+
+
+        //Taka út liðnar dagsetningar
+        datePicker.setDayCellFactory(picker -> new DateCell() {
+            @Override
+            public void updateItem(LocalDate date, boolean empty) {
+                super.updateItem(date, empty);
+                if (date.isBefore(LocalDate.now())) {
+                    setDisable(true);
+                    setStyle("-fx-background-color: #EEEEEE;");
+                }
+            }
+        });
 
         searchEngineController = new SearchEngineController();
         fxLeitarvelTexti.setOnKeyPressed(event -> {
